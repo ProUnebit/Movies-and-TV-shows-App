562f3be371c3a03f947dacecf56050e1312514df
ProUnebit <prounebit@yandex.ru>
2023-05-13 17:46:53 +0300

    - removed modules folder - relocate calc.js to utils folder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,15 @@ import { useSelector, useDispatch } from 'react-redux'
 import { getApiConfiguration, getGenres } from "../store/homeSlice";
 import { fetchDataFromApi } from '../utils/api'
 
+import Header from './Header/Header'
+import Footer from './Footer/Footer'
+import Home from '../pages/home/Home'
+import Details from '../pages/details/Details'
+import SearchResult from '../pages/searchResult/SearchResult'
+import Explore from '../pages/explore/Explore'
+import PageNotFound from '../pages/404/PageNotFound'
+
+
 
 
 const App = () => {
