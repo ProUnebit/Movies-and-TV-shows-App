9a5e0df3570b3da08b24214e867e8005e1ed60d5
ProUnebit <prounebit@yandex.ru>
2023-05-14 22:35:32 +0300

    added CircleRating comp

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 import ContentWrapper from '../../../components/contentWrapper/ContentWrapper'
 import SwitchTabs from '../../../components/switchTabs/SwitchTabs'
+import Carousel from '../../../components/carousel/Carousel'
 
 import useFetch from '../../../hooks/useFetch'
 
@@ -22,6 +23,7 @@ const Trending = () => {
                 <span className="carouselTitle">Trending</span>
                 <SwitchTabs data={['Day', 'Week']} onTabChange={onTabChange} />
             </ContentWrapper>
+            <Carousel data={data?.results} loading={loading} />
         </div>
     )
 }
