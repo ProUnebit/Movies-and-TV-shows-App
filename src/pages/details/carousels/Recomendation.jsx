6a8677e71e82e844bad16ed97ecf1d83b89a5edd
ProUnebit <prounebit@yandex.ru>
2023-05-17 23:40:48 +0300

    - fixed Similar & Recommendation render - edded correct Explore page render

--- src/pages/details/carousels/Recomendation.jsx
+++ src/pages/details/carousels/Recomendation.jsx
@@ -4,16 +4,22 @@ import Carousel from "../../../components/carousel/Carousel";
 import useFetch from "../../../hooks/useFetch";
 
 const Recommendation = ({ mediaType, id }) => {
-    
+
     const { data, loading, error } = useFetch(`/${mediaType}/${id}/recommendations`);
 
     return (
-        <Carousel
-            title="Recommendations"
-            data={data?.results}
-            loading={loading}
-            endpoint={mediaType}
-        />
+        <>
+            { data?.results.length > 0 ?
+                <Carousel
+                title="Recommendations"
+                data={data?.results}
+                loading={loading}
+                endpoint={mediaType}
+                />
+                :
+                null
+            }
+        </>
     );
 };
 
--- src/pages/details/carousels/Similar.jsx
+++ src/pages/details/carousels/Similar.jsx
@@ -4,18 +4,24 @@ import Carousel from "../../../components/carousel/Carousel";
 import useFetch from "../../../hooks/useFetch";
 
 const Similar = ({ mediaType, id }) => {
-    
+
     const { data, loading, error } = useFetch(`/${mediaType}/${id}/similar`);
 
     const title = mediaType === "tv" ? "Similar TV Shows" : "Similar Movies";
 
     return (
-        <Carousel
-            title={title}
-            data={data?.results}
-            loading={loading}
-            endpoint={mediaType}
-        />
+        <>
+            { data?.results.length > 0 ? 
+                <Carousel
+                title={title}
+                data={data?.results}
+                loading={loading}
+                endpoint={mediaType}
+                />
+                :
+                null
+            }
+        </>
     );
 };
 
